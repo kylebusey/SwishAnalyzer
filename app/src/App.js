fb2a9ae4a241d31cba4fdaf4a3c7df318dfb9e36
Kyle Busey <kyledbuseybusiness@gmail.com>
2023-11-18 18:02:24 -0500

    Converted homepage to React and added favicon

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import cslogo from "./assets/cs2-logo-scaled.jpg";
+import soldier from "./assets/soldier.jpg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="body-main">
       
+      <div className="title-info">
+        <h1 className="title">Swish Analyzer</h1>
+        <img src={cslogo} alt="CS2 Logo"/>
+        <h2 className="subtitle">Version 0.1</h2>
+      </div>
+
+      <div className="demo-data">
+        <button className="upload-button" id="upload">Upload Demo</button>
+        <p>Simply select your demo file and see the 
+            most crucial stats for your game.</p>
+      </div>
+
+      <div className="footer">
+
+      <img src={soldier} alt="CS2 Soldier"/>
+      <img src={soldier} alt="CS2 Soldier"/>
+      <img src={soldier} alt="CS2 Soldier"/>
+      <img src={soldier} alt="CS2 Soldier"/>
+      <img src={soldier} alt="CS2 Soldier"/>
+
+      </div>
     </div>
   );
 }
